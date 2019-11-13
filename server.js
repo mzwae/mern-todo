0f1b5dd4a3ae3673115c31e9aa3f19e5d8ffe917
mzwae <mrmonjaie@gmail.com>
2019-11-13 18:09:05 +0000

    Added server

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+// Bodyparser Middleware
+app.use(bodyParser.json());
+
+// DB Config
+const db = require('./config/keys').mongoURI;
+
+// Connect to Mongo
+mongoose
+.connect(db)
+.then(() => console.log('MongoDB Connected...'))
+.catch(err => console.log(err));
+
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log(`Server listening on port ${port}...`));
