48082fb52d4667eb5cd871d985c4e944e59bd695
mzwae <mrmonjaie@gmail.com>
2019-11-19 22:00:58 +0000

    Added reactjs

--- server.js
+++ server.js
@@ -22,6 +22,6 @@ mongoose
 
 // Use api routes
 app.use('/api/items', items);
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server listening on port ${port}...`));
