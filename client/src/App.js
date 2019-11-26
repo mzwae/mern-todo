27479946f54248b08d281b3f7e39c856e116893b
mzwae <mrmonjaie@gmail.com>
2019-11-26 18:28:56 +0000

    added reducers

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,20 @@
 import React, { Component } from 'react';
 import AppNavbar from './components/AppNavbar';
 import TodoList from './components/TodoList';
+import { Provider } from 'react-redux';
+import store './store';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <AppNavbar/>
-        <TodoList/>
-      </div>
+	    <Provider sotre={store}>
+		      <div className="App">
+		        <AppNavbar/>
+		        <TodoList/>
+		      </div>
+	    </Provider>
     );
   }
 }
