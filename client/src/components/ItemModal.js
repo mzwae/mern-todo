2b6bb8158508489ed4ce29a3d3092da253120aae
mzwae <mrmonjaie@gmail.com>
2019-11-30 21:51:14 +0000

    added itemModal

---
+++ client/src/components/ItemModal.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import {
+	Button,
+	Modal,
+	ModalHeader,
+	ModalBody,
+	Form,
+	FormGroup,
+	Label,
+	Input
+} from 'reactstrap';
+import { connect } from 'react-redux';
+import { addItem } from '../actions/itemActions';
+
+class ItemModal extends Component {
+
+	state = {
+		modal: false,
+		name: ''
+	}
+
+	toggle = () => {
+		this.setState({
+			modal: !this.state.modal
+		});
+	}
+
+	render(){
+		return(
+				<div>
+					
+				</div>
+			);
+	}
+
+}
+
+export default connect()(ItemModal);
