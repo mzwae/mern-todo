3546dd67f27abc29a84fc7dca6e0248c43c02598
mzwae <mrmonjaie@gmail.com>
2019-12-01 21:25:06 +0000

    added add-item modal

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import AppNavbar from './components/AppNavbar';
 import TodoList from './components/TodoList';
+import ItemModal from './components/ItemModal';
+
+
 import { Provider } from 'react-redux';
 import store './store';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -12,6 +15,7 @@ class App extends Component {
 	    <Provider sotre={store}>
 		      <div className="App">
 		        <AppNavbar/>
+            <ItemModal/>
 		        <TodoList/>
 		      </div>
 	    </Provider>
--- client/src/components/ItemModal.js
+++ client/src/components/ItemModal.js
@@ -25,10 +25,41 @@ class ItemModal extends Component {
 		});
 	}
 
+	onChange = (e) => {
+		this.setState({ [e.target.name]: e.target.value });
+	}
+
 	render(){
 		return(
 				<div>
-					
+					<Button
+					 	color="dark"
+						style={{marginBottom: '2rem'}}
+						onClick={this.toggle}
+					>
+						Add Item
+					</Button>
+					<Modal
+						isOpen={this.state.modal}
+						toggle={this.toggle}
+					>
+						<ModalHeader toggle={this.tggle}>Add item</ModalHeader>
+
+						<ModalBody>
+							<Form>
+								<FormGroup>
+									<Label for="item">Item</Label>
+									<Input
+										type="text"
+										name="name"
+										id="item"
+										placeholder="Add item"
+										onChange={this.onChange}
+									/>
+								</FormGroup>
+							</Form>
+						</ModalBody>
+					</Modal>
 				</div>
 			);
 	}
