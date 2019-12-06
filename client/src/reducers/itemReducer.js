a9e51790abac6e9a6c42038c0437b5cf40c9856f
mzwae <mrmonjaie@gmail.com>
2019-12-06 22:34:35 +0000

    Removed static values

--- client/src/reducers/itemReducer.js
+++ client/src/reducers/itemReducer.js
@@ -1,15 +1,19 @@
 import uuid from 'uuid';
-import { GET_ITEMS, ADD_ITEM, DELETE_ITEM } from '../actions/types';
+import { GET_ITEMS, ADD_ITEM, DELETE_ITEM, ITEMS_LOADING} from '../actions/types';
 
 const initialState = {
-	items: [
-			{id: uuid(), name: 'Have breakfast'},
-			{id: uuid(), name: 'Go to school'},
-			{id: uuid(), name: 'Meet maths professor'},
-			{id: uuid(), name: 'Do grocery'},
-			{id: uuid(), name: 'Go to Yoga class'},
-			{id: uuid(), name: 'Complete car insurance'}
-		]
+// 	items: [
+// 			{id: uuid(), name: 'Have breakfast'},
+// 			{id: uuid(), name: 'Go to school'},
+// 			{id: uuid(), name: 'Meet maths professor'},
+// 			{id: uuid(), name: 'Do grocery'},
+// 			{id: uuid(), name: 'Go to Yoga class'},
+// 			{id: uuid(), name: 'Complete car insurance'}
+// 		]
+// };
+const initialState = {
+	items: [],
+	loading: false
 };
 
 export default function(state = initialState, action){
@@ -23,13 +27,21 @@ export default function(state = initialState, action){
 		return {
 			...state,
 			items: state.items.filter(item => item.id !== action.payload)
-		}
+		};
 
 		case ADD_ITEM:
 			return {
 				...state,
 				items: [action.payload, ...state.items]
-			}
+			};
+
+			case ITEMS_LOADING:
+				return {
+					...state,
+					laoding: true
+				};
+
+
 
 		default:
 			return state;
