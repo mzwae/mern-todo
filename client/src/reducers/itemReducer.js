5827a2b7537a7fb8fe854350c1d9a81328cfc28b
mzwae <mrmonjaie@gmail.com>
2019-12-17 21:33:06 +0000

    removed initialstate

--- client/src/reducers/itemReducer.js
+++ client/src/reducers/itemReducer.js
@@ -1,15 +1,4 @@
 import { GET_ITEMS, ADD_ITEM, DELETE_ITEM, ITEMS_LOADING} from '../actions/types';
-
-// const initialState = {
-// 	items: [
-// 			{id: uuid(), name: 'Have breakfast'},
-// 			{id: uuid(), name: 'Go to school'},
-// 			{id: uuid(), name: 'Meet maths professor'},
-// 			{id: uuid(), name: 'Do grocery'},
-// 			{id: uuid(), name: 'Go to Yoga class'},
-// 			{id: uuid(), name: 'Complete car insurance'}
-// 		]
-// };
 const initialState = {
 	items: [],
 	loading: false
