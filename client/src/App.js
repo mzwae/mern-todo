181346a4140a9a8a9f1693ebe90d537c66df4e2e
mzwae <mrmonjaie@gmail.com>
2019-12-19 23:19:54 +0000

    fixed TypeError bug

--- client/src/App.js
+++ client/src/App.js
@@ -6,11 +6,14 @@ import { Container } from 'reactstrap';
 
 
 import { Provider } from 'react-redux';
+// import { createStore } from 'redux';
+// import reducer from './reducers/itemReducer'
 import store from './store';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 class App extends Component {
+  // const store = createStore(reducer);
   render() {
     return (
 	    <Provider sotre={store}>
